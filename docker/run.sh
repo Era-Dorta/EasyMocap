374e392fef567e6334090b5a9a0b9c22d808aea7
Era Dorta <e.dorta@tudelft.nl>
2024-03-05 15:24:10 +0000

    Fix rendering issues

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,6 @@
-docker run -it --gpus all -v $(pwd):/workspace local/easymocap:pytorch2.2.0-cuda12.1 /bin/bash
+docker run -it \
+       --gpus all -e DISPLAY=:1 \
+       -v /tmp/.X11-unix:/tmp/.X11-unix \
+       -v $(pwd):/workspace \
+       local/easymocap:pytorch2.2.0-cuda12.1 \
+       /bin/bash

5350279a4021b2c35a829b636dd79480eb2b234d
Era Dorta <e.dorta@tudelft.nl>
2024-08-15 15:45:19 +0200

    Fix data path for the entry point

--- container_scripts/docker_run.sh
+++ container_scripts/docker_run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-       # -v $(pwd):/workspace \
-       #-v /tmp/.X11-unix:/tmp/.X11-unix \
-       # -e DISPLAY=:1 
+if [ $# -ne 1 ]; then
+    echo "Must provide the path to the data folder as argument"
+    exit 1
+fi
 
 REPO_DIR=$(dirname $(dirname $(realpath $0)))
 
@@ -9,14 +10,25 @@ cd $REPO_DIR
 
 VERSION=$(grep -oP "(?<=    version=')[^']*" setup.py)
 
-docker run -it \
+DATA_PATH=$(readlink -f $1)
+
+docker run \
        --rm \
        --network=host \
-       --gpus '"device=0"' \
-       -v "$(pwd)"/data/examples:/home/user/easymocap/EasyMocap/data/examples \
-       -v "$(pwd)":/home/user/easymocap/EasyMocap \
-       eradorta/easymocap:$VERSION \
-       /bin/bash
+       --gpus all \
+       -v ${DATA_PATH}:/home/user/easymocap/EasyMocap/data/examples/_data \
+       eradorta/easymocap:$VERSION
 
+# Run in dev mode, including X11 flags for rendering and GUI stuff
+# docker run \
+#        -it \
+#        --rm \
+#        --network=host \
+#        --gpus all \
+#        -v "$(pwd)":/home/user/easymocap/EasyMocap \
+#        -v /tmp/.X11-unix:/tmp/.X11-unix \
+#        -e DISPLAY=:1 \
+#        -v ${DATA_PATH}:/home/user/easymocap/EasyMocap/data/examples/_data \
+#        eradorta/easymocap:$VERSION \
+#        bash
 
-# docker run -it --rm --gpus '"device=0"' -v "$(pwd)"/data/examples:/openpose/examples/easy cwaffles/openpose
--- container_scripts/entrypoint.sh
+++ container_scripts/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# data=/home/user/easymocap/EasyMocap/data/examples/revise_two_1_5_seconds/
-
-data=$1
+# This is the multi-vew multiple people video demo from https://github.com/zju3dv/EasyMocap/blob/master/doc/mvmp.md
+data=/home/user/easymocap/EasyMocap/data/examples/_data
 python scripts/preprocess/extract_video.py ${data} --no2d
 python apps/preprocess/extract_keypoints.py ${data} --mode openpose --hand --face
 python apps/demo/mvmp.py ${data} --out ${data}/output --annot annots --cfg config/exp/mvmp1f.yml --undis --vis_det --vis_repro

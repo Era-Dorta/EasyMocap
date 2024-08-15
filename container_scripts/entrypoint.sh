8c2aea2e1e55a58e2c2aff2957898f8e540961f2
Era Dorta <e.dorta@tudelft.nl>
2024-08-15 11:55:29 +0200

    Fix typo in entrypoint script

--- container_scripts/entrypoint.sh
+++ container_scripts/entrypoint.sh
@@ -5,4 +5,4 @@
 data=$1
 python scripts/preprocess/extract_video.py ${data} --no2d
 python apps/preprocess/extract_keypoints.py ${data} --mode openpose --hand --face
-python apps/demo/mvmp.py ${data} --out ${data}/output --annot annots ---cfg config/exp/mvmp1f.yaml --undis --vis_det --vis_repro
+python apps/demo/mvmp.py ${data} --out ${data}/output --annot annots --cfg config/exp/mvmp1f.yml --undis --vis_det --vis_repro

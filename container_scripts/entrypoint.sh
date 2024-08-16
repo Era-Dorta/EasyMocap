c8eb91ce8cde75d7d568165588cbcb3695fbdc65
Era Dorta <e.dorta@tudelft.nl>
2024-08-16 10:28:56 +0200

    Fix wrong working dir with apptainer

--- container_scripts/entrypoint.sh
+++ container_scripts/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# This is the multi-vew multiple people video demo from https://github.com/zju3dv/EasyMocap/blob/master/doc/mvmp.md
+# This script is the multiple view with multiple people video demo from
+# https://github.com/zju3dv/EasyMocap/blob/master/doc/mvmp.md
+
 data=/home/user/easymocap/EasyMocap/data/examples/_data
+
+# Apptainer ignores the WORKDIR from the Dockerfile, manually cd into the WORKDIR
+cd /home/user/easymocap/EasyMocap 
+
 python scripts/preprocess/extract_video.py ${data} --no2d
 python apps/preprocess/extract_keypoints.py ${data} --mode openpose --hand --face
 python apps/demo/mvmp.py ${data} --out ${data}/output --annot annots --cfg config/exp/mvmp1f.yml --undis --vis_det --vis_repro

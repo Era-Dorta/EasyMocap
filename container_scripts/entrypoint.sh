060b33507851396c511d250e0a92800caaf89fef
Era Dorta <e.dorta@tudelft.nl>
2024-10-15 15:27:25 +0200

    Fix maximum number of frames in video to frame

--- container_scripts/entrypoint.sh
+++ container_scripts/entrypoint.sh
@@ -8,6 +8,6 @@ data=/home/user/easymocap/EasyMocap/data/examples/_data
 # Apptainer ignores the WORKDIR from the Dockerfile, manually cd into the WORKDIR
 cd /home/user/easymocap/EasyMocap 
 
-python scripts/preprocess/extract_video.py ${data} --no2d
+python scripts/preprocess/extract_video.py ${data} --no2d --end 1e9
 python apps/preprocess/extract_keypoints.py ${data} --mode openpose --hand --face
 python apps/demo/mvmp.py ${data} --out ${data}/output --annot annots --cfg config/exp/mvmp1f.yml --undis --vis_det --vis_repro
